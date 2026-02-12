1b18e425f82a2d806c10d10846edf6409c32fa84
franklinJeevitha <42205008+franklinJeevitha@users.noreply.github.com>
2026-02-12 14:24:51 +0800

    Change static pod image to version 1.28.4
    Updated the image version for the static pod.

--- scripts/staticpod.sh
+++ scripts/staticpod.sh
@@ -1,2 +1,4 @@
 #To create pod definition file in a manifest folder, so kubelet can create and run the static pod
 kubectl run --restart=Never --image=busybox static-busybox --dry-run=client -o yaml --command -- sleep 1000 > /etc/kubernetes/manifests/static-busybox.yaml
+#Update the version of the image used
+kubectl run --restart=Never --image=busybox:1.28.4 static-busybox --dry-run=client -o yaml --command -- sleep 1000 > /etc/kubernetes/manifests/static-busybox.yaml
