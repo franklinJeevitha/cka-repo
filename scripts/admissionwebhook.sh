e66aec36ff7bd72d232c1df0c1472ff9851af13d
franklinJeevitha <42205008+franklinJeevitha@users.noreply.github.com>
2026-02-16 14:46:44 +0800

    Add comment for TLS secret creation in webhook-demo
    Added a comment to clarify the purpose of the script.

---
+++ scripts/admissionwebhook.sh
@@ -0,0 +1,4 @@
+#Create a TLS secret in webhook-demo namespace
+kubectl -n webhook-demo create secret tls webhook-server-tls \
+    --cert "/root/keys/webhook-server-tls.crt" \
+    --key "/root/keys/webhook-server-tls.key"
